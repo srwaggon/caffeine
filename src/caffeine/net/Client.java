a3436c336f9f4878dd4af9bbabe37880fe912132
Samuel Waggoner <samuel.waggoner@gmail.com>
2012-07-03 22:33:00 -0400

    Maybe on accident fixed client?

--- src/caffeine/net/Client.java
+++ src/caffeine/net/Client.java
@@ -7,30 +7,31 @@ import caffeine.entity.Entity;
 import caffeine.view.GUI;
 import caffeine.view.screen.WorldScreen;
 import caffeine.world.Map;
+import caffeine.world.World;
 
 public class Client {
   protected HashMap<Integer, Entity> entities = new HashMap<Integer, Entity>();
   protected GUI gui;
   protected Connection host;
   protected NetworkInputHandler input;
-  
+
   public static void main(String[] args) {
     new Client("127.0.0.1", 4444).run();
   }
-  
+
   /* Constructor */
   public Client(String ip, int port) {
     host = new Connection(ip, port);
     input = new NetworkInputHandler(host);
     gui = new GUI("Caffeine Client");
-    gui.addInputHandler(input);
+    gui.addInputListener(input);
   }
-  
+
   public void run() {
     new Thread(gui).start();
     try {
       String query = "get map 0";
-      
+
       String response = "";
       Scanner scan = new Scanner(System.in);
       while (host.isConnected()) {
@@ -39,28 +40,29 @@ public class Client {
         response = host.read();
         Thread.sleep(10);
       }
-      
+
     } catch (InterruptedException e) {
-      
+
     }
   }
-  
+
   public void processServerResponse(String response) {
     Scanner lineParser = new Scanner(response);
     System.out.println(response);
     while (lineParser.hasNext()) {
       String strmap = lineParser.next();
       String mapID = lineParser.next();
-      
+
       String mapData = lineParser.nextLine();
       System.out.println(mapData);
-      
-      Map map = new Map(mapData);
+      World world = new World();
+      world.addMap(mapData);
+      Map map = world.getMap(0);
       System.out.println(map);
-      
+
       WorldScreen ws = new WorldScreen();
       ws.setMap(map);
-      
+
       gui.setScreen(ws);
     }
   }
