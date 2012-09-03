9bc9e4faa35298bd3ce5dedfc463d9c59896ee54
Samuel Waggoner <samuel.waggoner@gmail.com>
2012-09-03 10:28:11 -0400

    Smaller pushes...

--- src/caffeine/entity/Entity.java
+++ src/caffeine/entity/Entity.java
@@ -89,10 +89,10 @@ public class Entity {
       if (entity.intersects(nx - xr, ny - yr, nx + xr, ny + yr)) {
         if (!entity.touchedBy(this)) {
 
-          if (dir == Dir.UP) entity.knockback(0, entity.loc.y - loc.y);
-          if (dir == Dir.DOWN) entity.knockback(0, entity.loc.y - loc.y);
-          if (dir == Dir.LEFT) entity.knockback(entity.loc.x - loc.x, 0);
-          if (dir == Dir.RIGHT) entity.knockback(entity.loc.x - loc.x, 0);
+          if (dir == Dir.UP) entity.knockback(0, -1);
+          if (dir == Dir.DOWN) entity.knockback(0, 1);
+          if (dir == Dir.LEFT) entity.knockback(-1, 0);
+          if (dir == Dir.RIGHT) entity.knockback(1, 0);
 
           return false;
         }
