01fed783086b96ca4d7353e36a2793357af31951
Samuel Waggoner <samuel.waggoner@gmail.com>
2014-01-25 13:13:06 -0600

    Removes comments and bi-cardinal directions

--- src/caffeine/world/Dir.java
+++ src/caffeine/world/Dir.java
@@ -2,13 +2,9 @@ package caffeine.world;
 
 public enum Dir {
   N(0, -1),
-  NE(1, -1),
   E(1, 0),
-  SE(1, 1),
   S(0, 1),
-  SW(-1, 1),
-  W(-1, 0),
-  NW(-1, -1);
+  W(-1, 0);
   
   public final int dx;
   public final int dy;
@@ -23,41 +19,21 @@ public enum Dir {
         % Dir.values().length];
   }
   
-  /**
-   * Returns the next direction from this enumeration from the following order:
-   * N, E, S, W
-   * 
-   * @return The next direction from this enumeration
-   */
   public Dir next() {
     return Dir.values()[(ordinal() + 1) % Dir.count()];
   }
   
-  /**
-   * Returns the previous direction from this enumeration from the following
-   * order: W, S, E, N
-   * 
-   * @return The previous direction from this enumeration
-   */
   public Dir prev() {
     return ordinal() > 0 ? Dir.values()[ordinal() - 1] : Dir.values()[Dir
         .count() - 1];
   }
   
-  /**
-   * Returns a random direction.
-   * 
-   * @return randomly selected direction
-   */
+
   public static Dir pickOneAtRandom() {
     return Dir.values()[((int) Math.random() * Dir.count())];
   }
   
-  /**
-   * Returns the count of directions. Should be 4.
-   * 
-   * @return the count of directions.
-   */
+
   public static int count() {
     return Dir.values().length;
   }
